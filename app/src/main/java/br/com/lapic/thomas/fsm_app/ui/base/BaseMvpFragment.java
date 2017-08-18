c0850c2b6fbd0fd4f50cd9f5bc3817610a3c396f
Thomas <msamoht16@gmail.com>
2017-08-18 15:37:45 -0300

    Corrige aplicacao de dependencias do dagger 2

--- app/src/main/java/br/com/lapic/thomas/fsm_app/ui/base/BaseMvpFragment.java
+++ app/src/main/java/br/com/lapic/thomas/fsm_app/ui/base/BaseMvpFragment.java
@@ -6,6 +6,11 @@ import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
 import com.hannesdorfmann.mosby.mvp.MvpFragment;
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
+import br.com.lapic.thomas.fsm_app.MyApplication;
+import br.com.lapic.thomas.fsm_app.injection.component.ActivityComponent;
+import br.com.lapic.thomas.fsm_app.injection.component.DaggerActivityComponent;
+import br.com.lapic.thomas.fsm_app.injection.module.ActivityModule;
+
 /**
  * Created by thomas on 02/08/17.
  */
@@ -13,9 +18,24 @@ import com.hannesdorfmann.mosby.mvp.MvpView;
 public abstract class BaseMvpFragment<V extends MvpView, P extends MvpBasePresenter<V>>
         extends MvpFragment<V, P> {
 
+    private ActivityComponent mActivityComponent;
+
     @Override
     public void onAttach(Context context) {
+        injectComponent();
         super.onAttach(context);
     }
+
+    private void injectComponent() {
+        if (mActivityComponent == null) {
+            mActivityComponent = DaggerActivityComponent.builder()
+                    .activityModule(new ActivityModule(getActivity()))
+                    .applicationComponent(MyApplication.get(getActivity()).getComponent())
+                    .build();
+        }
+        inject(mActivityComponent);
+    }
+
+    public abstract void inject(ActivityComponent activityComponent);
 
 }
